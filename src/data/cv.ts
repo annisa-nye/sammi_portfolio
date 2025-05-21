621262239b3069a04c6a9b0a714d6bf69cb023fb
annisa-nye <annisanye99@gmail.com>
2025-05-21 14:49:40 +1000

    consolidatedcv.ts into 1 file

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,4 +1,15 @@
-export const cv = {
+export interface CVItem {
+	year: number;
+	items: string[];
+}
+
+export interface CV {
+	exhibitions: CVItem[];
+	residencies: CVItem[];
+	collaborations: CVItem[];
+}
+
+export const cv: CV = {
 	exhibitions: [
 		{
 			year: 2021,
