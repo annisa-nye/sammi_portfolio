0e6391ae62d119a720f939ac2b8e32c5bd31fb74
annisa-nye <annisanye99@gmail.com>
2025-09-08 15:42:30 +1000

    added aws deployment tingz

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,16 +1,1 @@
-globalThis.__BUILD_MANIFEST = {
-  "pages": {
-    "/_app": []
-  },
-  "devFiles": [],
-  "ampDevFiles": [],
-  "polyfillFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "ampFirstPages": []
-};
-globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8fc39b2d2d9b0b9d.js","static/chunks/4bd1b696-656d2469c3780413.js","static/chunks/684-62b9b1cdb4a506ea.js","static/chunks/main-app-02dd0351b298b75f.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-8fc39b2d2d9b0b9d.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-e065448390d258e2.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-8fc39b2d2d9b0b9d.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-e065448390d258e2.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CeiI/L2LhLgYjaUmjOPSvMHabVW8bzWUWi5cO+pNp2k=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
